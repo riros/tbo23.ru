177a12e967d93c816ad33978e133b1eae38a887d
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-22 10:21:15 +0300

    save

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd /var/www/tbo23.ru/
+sudo git pull
+sudo chown www-data:www-data -R ./*
+
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
+#python manage.py thumbnail clear_delete_all
+#systemctl restart uwsgi-app@lanzeva
+systemctl restart uwsgi
+systemctl restart nginx
+sudo chown www-data:www-data -R ./*
