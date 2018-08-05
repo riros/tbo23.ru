3a631091d60aab1a39e79a7ba7c1d200ef9b63b6
riros server <riros@tbo23.ru>
2018-08-05 15:56:40 +0300

    save

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 cd /var/www/tbo23.ru/
 sudo git pull
-sudo chown www-data:www-data -R /var/www/tbo23.ru
+sudo chown nginx:nginx -R /var/www/tbo23.ru/
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 #python manage.py thumbnail clear_delete_all
-#systemctl restart uwsgi-app@lanzeva
-systemctl restart uwsgi
+#systemctl restart uwsgi-app@tbo23
+systemctl restart uwsgi-app@tbo23.ru
 systemctl restart nginx
-sudo chown nginx:nginx -R /var/www/tbo23.ru
---
+++ update_acc.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget http://srv.tbo23.ru/distrib/exp.json
